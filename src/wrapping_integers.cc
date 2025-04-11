938b22e9097b61968d33e1ead6ccacdbf2e2282f
junior <1508651338@qq.com>
2025-04-11 14:10:16 +0800

    impl wrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -6,13 +6,20 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  // debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
+  return Wrap32 { zero_point.raw_value_+static_cast<uint32_t>(n)};
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  // debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
+  Wrap32 warpCheckpoint=wrap(checkpoint,zero_point);
+  uint32_t diff=raw_value_-warpCheckpoint.raw_value_;
+  const uint64_t upper=static_cast<uint64_t>(UINT32_MAX)+1;
+  if(diff<=(upper>>1)||checkpoint+diff<upper)
+  {
+    return checkpoint+diff;
+  }
+  return checkpoint+diff-upper;
 }
